c143c408766e8427c68d771327a84dda1429026d
Anne Zhou <annezhou@ugps-MacBook-6.local>
2018-02-02 22:25:12 -0800

    skeleton python file

---
+++ model.py
@@ -0,0 +1,14 @@
+# CS155: Miniproject 1
+# Kavya Sreedhar, Audrey Wang, Anne Zhou
+
+import numpy as np 
+import tensorflow as tf 
+import keras
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+
+# Read in data.
+
+# Construct model.
+
+# Profit. (generate predictions)
